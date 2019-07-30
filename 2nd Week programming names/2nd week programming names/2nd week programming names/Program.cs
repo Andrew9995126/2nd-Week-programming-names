1ef43398c292437671e04aff44d193f56a155ad9
Andrew Arther <9995126@student.toiohomai.ac.nz>
2019-07-31 11:38:02 +1200

    commit 1

---
+++ 2nd Week programming names/2nd week programming names/2nd week programming names/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_week_programming_names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your first name: ");
+            string name1 = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name: ");
+            string name2 = Console.ReadLine();
+
+            Console.WriteLine($"This is your full name: {name1} {name2}");
+            Console.ReadLine();
+        }
+    }
+}
